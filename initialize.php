41b7ded0164109fc94d0ae360c5d012cba28a026
smartwiziyk <ike_genius1@yahoo.com>
2023-03-29 16:22:50 +0100

    database name

--- initialize.php
+++ initialize.php
@@ -1,10 +1,10 @@
 <?php
-$dev_data = array('id'=>'-1','firstname'=>'Ikenna','lastname'=>'','username'=>'admin','password'=>'827ccb0eea8a706c4c34a16891f84e7b','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/scheduler/');
+$dev_data = array('id'=>'-1','firstname'=>'Ikenna','lastname'=>'','username'=>'admin ike','password'=>'827ccb0eea8a706c4c34a16891f84e7b','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','http://localhost/ttschedulersec/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"scheduler_db");
+if(!defined('DB_NAME')) define('DB_NAME',"scheduler_db_secured");
 ?>
